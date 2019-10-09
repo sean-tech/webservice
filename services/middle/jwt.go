06fe151e0830f4ab781d41163facb7876517aad9
sean <1028990481@qq.com>
2019-10-09 11:11:36 +0800

    jwt sso add

--- services/middle/jwt.go
+++ services/middle/jwt.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sean-tech/webservice/config"
 	"github.com/sean-tech/webservice/services"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,9 @@ type Claims struct {
 	Password string `json:"password"`
 	jwt.StandardClaims
 }
+
+// 用户token存储映射，用户当前token唯一性保证
+var userCurrentTokenMap sync.Map
 
 func GenerateToken(userId uint64, userName, password string) (string, error) {
 	expireTime := time.Now().Add(3 * time.Hour)
@@ -31,6 +35,9 @@ func GenerateToken(userId uint64, userName, password string) (string, error) {
 	}
 	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	token, err := tokenClaims.SignedString(jwtSecret)
+	if err == nil {
+		userCurrentTokenMap.Store(userId, token)
+	}
 	return token, err
 }
 
@@ -49,7 +56,6 @@ func ParseToken(token string) (*Claims, error) {
 func Jwt() gin.HandlerFunc {
 	handler := func(ctx *gin.Context) {
 		g := services.Gin{ctx}
-		var statusCode services.StatusCode = services.STATUS_CODE_SUCCESS
 		// token
 		token := ctx.GetHeader("Authorization")
 		if token == "" {
@@ -60,19 +66,26 @@ func Jwt() gin.HandlerFunc {
 		// token parse
 		claims, err := ParseToken(token)
 		if err != nil {
-			statusCode = services.STATUS_CODE_AUTH_CHECK_TOKEN_FAILED
-		} else if time.Now().Unix() > claims.ExpiresAt {
-			statusCode = services.STATUS_CODE_AUTH_CHECK_TOKEN_TIMEOUT
-		} else {
-			ctx.Set(services.KEY_CTX_USERID, claims.UserId)
-			ctx.Set(services.KEY_CTX_USERNAME, claims.UserName)
-			ctx.Set(services.KEY_CTX_PASSWORD, claims.Password)
-		}
-		if statusCode != services.STATUS_CODE_SUCCESS {
-			g.ResponseCode(statusCode, nil)
+			g.ResponseCode(services.STATUS_CODE_AUTH_CHECK_TOKEN_FAILED, nil)
 			ctx.Abort()
 			return
 		}
+		// time judge
+		if time.Now().Unix() > claims.ExpiresAt {
+			g.ResponseCode(services.STATUS_CODE_AUTH_CHECK_TOKEN_TIMEOUT, nil)
+			ctx.Abort()
+			return
+		}
+		// current token union judge
+		if savedToken, ok := userCurrentTokenMap.Load(claims.UserId); !ok || savedToken != token {
+			g.ResponseCode(services.STATUS_CODE_AUTH_CHECK_TOKEN_TIMEOUT, nil)
+			ctx.Abort()
+			return
+		}
+		// transfer
+		ctx.Set(services.KEY_CTX_USERID, claims.UserId)
+		ctx.Set(services.KEY_CTX_USERNAME, claims.UserName)
+		ctx.Set(services.KEY_CTX_PASSWORD, claims.Password)
 		// next
 		ctx.Next()
 	}

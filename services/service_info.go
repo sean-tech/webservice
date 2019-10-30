7a1f1fde02645705d1f373efb97f15c857ae6d51
sean-tech <1028990481@qq.com>
2019-10-30 17:08:06 +0800

    add serviceInfo check

--- services/service_info.go
+++ services/service_info.go
@@ -16,6 +16,9 @@ type ServiceInfo struct {
 	IsAdministrotor bool 	`json:"isAdministrotor"`
 }
 
+/**
+ * 新建context，并初始化info，绑定serviceId
+ */
 func NewContext(parentCtx context.Context) context.Context {
 	id, _ := GenerateId(config.AppSetting.WorkerId)
 	info := &ServiceInfo{
@@ -29,10 +32,30 @@ func NewContext(parentCtx context.Context) context.Context {
 	return context.WithValue(parentCtx, KEY_SERVICE_INFO, info)
 }
 
+/**
+ * 信息获取，获取传输链上context绑定的用户服务信息
+ */
 func GetServiceInfo(ctx context.Context) *ServiceInfo {
 	obj := ctx.Value(KEY_SERVICE_INFO)
 	if info, ok := obj.(*ServiceInfo); ok {
 		return  info
 	}
 	return nil
+}
+
+/**
+ * 信息校验，token绑定的用户信息同参数传入信息校验，信息不一致说明恶意用户传他人数据渗透
+ */
+func ServiceInfoCheck(ctx context.Context, userId uint64, userName string) bool {
+	info := GetServiceInfo(ctx)
+	if info == nil {
+		return false
+	}
+	if info.IsAdministrotor {
+		return true
+	}
+	if info.UserId != userId || info.UserName != userName {
+		return false
+	}
+	return true
 }
